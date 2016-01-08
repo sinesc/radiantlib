1da70c5e937660928007790a1a1e4b8ea0f45e7e
Dennis Möhlmann <dmo479@gmail.com>
2016-01-08 01:01:13 +0100

    support non-power-of-2 texture-wrapping, fix invalid attribute pointers after buffer-switch

--- radiant/shader/plain.fs
+++ radiant/shader/plain.fs
@@ -2,12 +2,30 @@
 precision mediump float;
 
 uniform sampler2D uTexture;
+uniform vec2 uTextureMaxUV;
 
 varying vec2 vTextureCoord;
 varying vec4 vColor;
 
 void main(void) {
 
-    //gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
-    gl_FragColor = texture2D(uTexture, vTextureCoord) * vColor;
+/*
+    // debug: colorize background
+
+    gl_FragColor = texture2D(
+                        uTexture,
+                        vec2(   // wrap texture at max used u/v
+                            fract(vTextureCoord.x / uTextureMaxUV.x) * uTextureMaxUV.x,
+                            fract(vTextureCoord.y / uTextureMaxUV.y) * uTextureMaxUV.y
+                         )
+                    ) * vColor + (vColor / 3.0);
+*/
+
+    gl_FragColor = texture2D(
+                        uTexture,
+                        vec2(   // wrap texture at max used u/v
+                            fract(vTextureCoord.x / uTextureMaxUV.x) * uTextureMaxUV.x,
+                            fract(vTextureCoord.y / uTextureMaxUV.y) * uTextureMaxUV.y
+                         )
+                    ) * vColor;
 }
